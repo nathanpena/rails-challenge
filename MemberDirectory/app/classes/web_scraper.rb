a4714c8a6dd4bee7505da4cc799182b4fe656477
Nathan Pena <nnpena@outlook.com>
2017-09-21 11:23:23 -0400

    Update scraper to remove non-alphanumberic characters as that can break things.

--- MemberDirectory/app/classes/web_scraper.rb
+++ MemberDirectory/app/classes/web_scraper.rb
@@ -9,8 +9,8 @@ class WebScraper
 
   def scrape_site_headings
     doc = Nokogiri::HTML(open(site_url).read)
-    site_headings[:h1] = doc.css('h1').children.map {|child| child.text}
-    site_headings[:h2] = doc.css('h2').children.map {|child| child.text}
-    site_headings[:h3] = doc.css('h3').children.map {|child| child.text}
+    ['h1', 'h2', 'h3'].each do |header_type|
+      site_headings[header_type.to_sym] = doc.css(header_type).children.map {|child| child.text.gsub(/[^0-9a-z ]/i, '')}
+    end
   end
 end
