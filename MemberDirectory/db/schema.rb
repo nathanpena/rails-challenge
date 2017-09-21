a453f0a1f067e2787c48daaae43deade65bbd3f7
Nathan Pena <nnpena@outlook.com>
2017-09-21 09:57:59 -0400

    Add friendship gem and migrations.

--- MemberDirectory/db/schema.rb
+++ MemberDirectory/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921135503) do
+ActiveRecord::Schema.define(version: 20170921135726) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "friendable_id"
+    t.string   "friendable_type"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "blocker_id"
+    t.integer  "status"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
